7523115212703ae324d9ebf839b6bd9dacce74b8
Luis moreira <luis.moreira@accenture.com>
2025-07-03 18:57:44 +0000

    [fix]: fixing problems involving deployment

---
+++ app/gabiarra-do-trigo/webapp/test/integration/FirstJourney.js
@@ -0,0 +1,38 @@
+sap.ui.define([
+    "sap/ui/test/opaQunit"
+], function (opaTest) {
+    "use strict";
+
+    var Journey = {
+        run: function() {
+            QUnit.module("First journey");
+
+            opaTest("Start application", function (Given, When, Then) {
+                Given.iStartMyApp();
+
+                Then.onTheColetasList.iSeeThisPage();
+
+            });
+
+
+            opaTest("Navigate to ObjectPage", function (Given, When, Then) {
+                // Note: this test will fail if the ListReport page doesn't show any data
+                
+                When.onTheColetasList.onFilterBar().iExecuteSearch();
+                
+                Then.onTheColetasList.onTable().iCheckRows();
+
+                When.onTheColetasList.onTable().iPressRow(0);
+                Then.onTheColetasObjectPage.iSeeThisPage();
+
+            });
+
+            opaTest("Teardown", function (Given, When, Then) { 
+                // Cleanup
+                Given.iTearDownMyApp();
+            });
+        }
+    }
+
+    return Journey;
+});
